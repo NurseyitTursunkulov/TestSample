b47c322c4840417557a8e9150396c4a1bbf91ebc
NurseyitTursunkulov <nurseyit.tursunkukulov@gmail.com>
2020-02-10 21:32:15 +0600

    changed adpater

--- app/src/main/java/com/example/testrussia/news/NewsAdapter.kt
+++ app/src/main/java/com/example/testrussia/news/NewsAdapter.kt
@@ -1,17 +1,45 @@
 package com.example.testrussia.news
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testrussia.databinding.NewsItemBinding
 import com.example.testrussia.news.model.NewsModel
 
-class NewsAdapter : PagedListAdapter<NewsModel, NewsItemViewHolder>(diffCallback) {
-    override fun onBindViewHolder(holder: NewsItemViewHolder, position: Int) {
-        holder.bindTo(getItem(position))
+class NewsAdapter(private val viewModel: NewsViewModel) :  ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffCallback) {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+
+        holder.bind(viewModel, item)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsItemViewHolder =
-        NewsItemViewHolder(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    class ViewHolder private constructor(val binding: NewsItemBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(viewModel: NewsViewModel, newsModel: NewsModel) {
+
+            binding.viewmodel = viewModel
+            binding.newsItem = newsModel
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = NewsItemBinding.inflate(layoutInflater, parent, false)
+
+                return ViewHolder(binding)
+            }
+        }
+    }
 
     companion object {
 
